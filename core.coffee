4cadb14a7f24d0655473c5e0964c8e5588fd96af
antimatter15 <antimatter15@gmail.com>
2012-02-12 11:01:19 -0500

    giving up again

--- core.coffee
+++ core.coffee
@@ -5,12 +5,36 @@ window.parse = (ab) ->
 		ptr += num
 	read = (num) ->
 		val = 0
-		for n in [0...num]
-			console.log arr[ptr+n], (ptr+n).toString(16)
-			val = (val * 256) + arr[ptr+n] 
+		ptr += num
+		for n in [1..num]
+			#console.log arr[ptr+n], (ptr+n).toString(16)
+			val = (val * 256) + arr[ptr-n] 
 		val
-	console.log read 8
-	ptr = 76
-	console.log read 4
+	_abSig = read 8
+	_clsid = read 16
+	_uMinorVersion = read 2
+	_uDllVersion = read 2
+	_uByteOrder = read 2
+	throw "Wrong endianness." if _uByteOrder is not 0xfffe
+	_uSectorShift = read 2
+	console.log _uSectorShift
+	_uMiniSectorShift = read 2
+	_usReserved = read 2
+	_ulReserved1 = read 4
+	_csectDir = read 4
+	_csectFat = read 4
+	_sectDirStart = (read 4) >> 0
+	_signature = read 4
+	_ulMiniSectorCutoff = read 4
+	_sectMiniFatStart = (read 4) >> 0
+	_csectMiniFat = read 4
+	_sectDifStart = (read 4) >> 0
+	console.log _sectDirStart, _sectMiniFatStart, _sectDifStart
+	_csectDif = read 4
+	console.log _csectDif
+	_sectFat = ((read 4) >> 0 for i in [0...109])
+
+	console.log _sectFat
+	
 	
 	
--- core.js
+++ core.js
@@ -1,6 +1,6 @@
 (function() {
   window.parse = function(ab) {
-    var arr, ptr, read, skip;
+    var arr, i, ptr, read, skip, _abSig, _clsid, _csectDif, _csectDir, _csectFat, _csectMiniFat, _sectDifStart, _sectDirStart, _sectFat, _sectMiniFatStart, _signature, _uByteOrder, _uDllVersion, _uMiniSectorShift, _uMinorVersion, _uSectorShift, _ulMiniSectorCutoff, _ulReserved1, _usReserved;
     ptr = 0;
     arr = new Uint8Array(ab);
     skip = function(num) {
@@ -9,14 +9,44 @@
     read = function(num) {
       var n, val;
       val = 0;
-      for (n = 0; 0 <= num ? n < num : n > num; 0 <= num ? n++ : n--) {
-        console.log(arr[ptr + n], (ptr + n).toString(16));
-        val = (val * 256) + arr[ptr + n];
+      ptr += num;
+      for (n = 1; 1 <= num ? n <= num : n >= num; 1 <= num ? n++ : n--) {
+        val = (val * 256) + arr[ptr - n];
       }
       return val;
     };
-    console.log(read(8));
-    ptr = 76;
-    return console.log(read(4));
+    _abSig = read(8);
+    _clsid = read(16);
+    _uMinorVersion = read(2);
+    _uDllVersion = read(2);
+    _uByteOrder = read(2);
+    if (_uByteOrder === !0xfffe) {
+      throw "Wrong endianness.";
+    }
+    _uSectorShift = read(2);
+    console.log(_uSectorShift);
+    _uMiniSectorShift = read(2);
+    _usReserved = read(2);
+    _ulReserved1 = read(4);
+    _csectDir = read(4);
+    _csectFat = read(4);
+    _sectDirStart = (read(4)) >> 0;
+    _signature = read(4);
+    _ulMiniSectorCutoff = read(4);
+    _sectMiniFatStart = (read(4)) >> 0;
+    _csectMiniFat = read(4);
+    _sectDifStart = (read(4)) >> 0;
+    console.log(_sectDirStart, _sectMiniFatStart, _sectDifStart);
+    _csectDif = read(4);
+    console.log(_csectDif);
+    _sectFat = (function() {
+      var _results;
+      _results = [];
+      for (i = 0; i < 109; i++) {
+        _results.push((read(4)) >> 0);
+      }
+      return _results;
+    })();
+    return console.log(_sectFat);
   };
 }).call(this);
